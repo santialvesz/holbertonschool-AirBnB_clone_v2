1679530782a0a4c70fffd3324cf63fbc273c9611
Santiago Alves <5102@holbertonstudents.com>
2022-11-30 12:54:57 -0300

    Task 3, using variables

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,7 +19,7 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes=False)
 def text(text):
-    return "C" + text.replace("_", " ")
+    return "C " + text.replace("_", " ")
 
 
 if __name__ == '__main__':
