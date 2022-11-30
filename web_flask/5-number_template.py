b053b64aafc1bd6edbd42bda42f04440d2f21c28
Santiago Alves <5102@holbertonstudents.com>
2022-11-30 13:39:38 -0300

    Task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,5 +33,10 @@ def number(n):
     return "{} is a number" .format(n)
 
 
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def numbertemplate(n):
+    return render_template("5-number.html", n=n)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
