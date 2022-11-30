c8ab2580d69f184c7f50980654a83571d675c11b
Santiago Alves <5102@holbertonstudents.com>
2022-11-30 11:55:35 -0300

    Fix pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """ script that starts a Flask web application
 """
-
-
 from flask import Flask
 
 
@@ -15,4 +13,4 @@ def index():
 
 
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
